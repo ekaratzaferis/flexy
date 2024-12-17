1dab03a5fa98c09afe5a47c5a62fcecab6fb9230
Alexandros Karatzaferis <ekaratzaferis@gmail.com>
2024-12-17 16:52:57 +0200

    add flag

--- examples/area-helper/area.js
+++ examples/area-helper/area.js
@@ -786,6 +786,7 @@ let tube1; let tube2; let ring; let ringG; let plane1; let plane2; let sphere; l
             'boundingBox': {
                 'dimX': settings.curve1.curveLength,
                 'dimZ': settings.curve2.curveLength,
+                'trueDimensions': true,
                 'type': 'square'
             },
             'inscribedShape': {}
@@ -797,6 +798,7 @@ let tube1; let tube2; let ring; let ringG; let plane1; let plane2; let sphere; l
             'boundingBox': {
                 'dimX': settings.curve1.curveLength,
                 'dimZ': settings.curve2.curveLength,
+                'trueDimensions': true,
                 'type': 'square'
             },
             'inscribedShape': {}
